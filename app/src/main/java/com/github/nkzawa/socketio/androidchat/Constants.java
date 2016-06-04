07f628f9c0ef3e6dac1d0c321639e3d324784031
R. Harish Navnit <harishnavnit@gmail.com>
2016-06-04 16:47:52 +0530

    Add new server URL

--- app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
@@ -1,5 +1,6 @@
 package com.github.nkzawa.socketio.androidchat;
 
 public class Constants {
-    public static final String CHAT_SERVER_URL = "http://chat.socket.io";
+    public static final String SERVER_URL = "http://192.168.43.102:3000";
+    //public static final String CHAT_SERVER_URL = "http://chat.socket.io";
 }
