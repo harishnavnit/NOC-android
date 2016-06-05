c5353eeb5165557b4bfc723196331d556038fc19
R. Harish Navnit <harishnavnit@gmail.com>
2016-06-05 17:50:33 +0530

    Minor additions

--- app/src/main/java/com/github/nkzawa/socketio/androidchat/MainActivity.java
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/MainActivity.java
@@ -1,6 +1,8 @@
 package com.github.nkzawa.socketio.androidchat;
 
+import android.app.Notification;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 
 
